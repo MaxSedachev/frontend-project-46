ad132061a70ce32611f736a450c9751f8b3e4bca
msedachev <msedachev@mts.ru>
2024-07-24 00:34:54 +0300

    modified file gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
-
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
 program.parse();
