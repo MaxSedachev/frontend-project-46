931ab782971f5cf377d1a7e70be63074e5ee242c
msedachev <msedachev@mts.ru>
2024-08-07 01:39:03 +0300

    mplemented a search for differences between two json files and added asciinema

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/MaxSedachev/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/MaxSedachev/frontend-project-46/actions)
+
+[Поиск различий между двумя json-файлами](https://asciinema.org/a/oxVYv2SObCTfAWrh7N1TAhnbw)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
+import { findDifferences } from "./differences.js";
 
 const getPath = (filePath) => path.resolve(process.cwd(), filePath);
 const getFile = (filePath) => fs.readFileSync(filePath, "utf8");
@@ -15,8 +16,13 @@ const getDiff = (filepath1, filepath2) => {
 
     const parsedFile1 = parse(file1)
     const parsedFile2 = parse(file2)
-    console.log("parsedFile1:", parsedFile1)
-    console.log("parsedFile2:", parsedFile2)
+    // Проверка парсинга данных из файлов (шаг-3)
+    // console.log("parsedFile1:", parsedFile1)
+    // console.log("parsedFile2:", parsedFile2)
+    const result = findDifferences(parsedFile1, parsedFile2);
+    // Проверка поиска различий между двумя json-файлами (шаг-4)
+    console.log('result:', result);
+    return result;
 };
 
 export default getDiff;
