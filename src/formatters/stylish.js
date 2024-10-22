d124ecac14803727f26742eeeb5b0bef16b9f270
msedachev <msedachev@mts.ru>
2024-10-23 00:48:15 +0300

    fix-13

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,7 +8,7 @@ const stylish = (file, replacer = ' ', spaceCount = 2) => {
         if (!_.isObject(obj1)) return `${obj1}`;
         const test = Object.entries(obj1).map(([key, value]) => {
           const preparedValue = iter1(value, depth1 + 2);
-          const indent = replacer.repeat(depth1 * spaceCount).slice(0, -2);
+          const indent = replacer.repeat(depth1 * spaceCount);
           return `${indent}${key}: ${preparedValue}`;
         });
         const outIndent = replacer.repeat((depth1 * spaceCount) - spaceCount);
