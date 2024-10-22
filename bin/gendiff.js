f77e3658198b4f4fee833fa24cf0b3404a721c9c
msedachev <msedachev@mts.ru>
2024-10-23 01:40:01 +0300

    fix-17

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,7 +12,7 @@ program
   .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2, option) => (
-    console.log(getDiff(filepath1, filepath2, option.format))));
+  .action((filepath1, filepath2) => (
+    console.log(getDiff(filepath1, filepath2, program.opts().format))));
 
-program.parse();
+program.parse(process.argv);
