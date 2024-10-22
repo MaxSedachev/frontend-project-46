4f2fbb70e569c2240a800820e6adf951a36a8d0b
msedachev <msedachev@mts.ru>
2024-10-23 02:29:54 +0300

    fix-20

--- src/differences.js
+++ src/differences.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 
 const findDifferences = (obj1, obj2) => {
-  const keys = _.union(_.keys(obj1), _.keys(obj2)).sort();
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const sortKeys = [...keys].sort();
 
-  return keys.map((key) => {
+  return sortKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
